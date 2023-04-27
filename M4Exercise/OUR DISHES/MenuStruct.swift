bc71857f637c76c71e62d0fbaeea58c5ca37d7a6
Glendon Philipp Baculio <glendonphilippbaculio@gmail.com>
2023-04-28 03:13:29 +0800

    exercise 1

--- M4Exercise/OUR DISHES/MenuStruct.swift
+++ M4Exercise/OUR DISHES/MenuStruct.swift
@@ -2,6 +2,7 @@ import Foundation
 
 struct JSONMenu: Codable {
     // add code here
+    let menu:  [MenuItem]
 }
 
 
@@ -9,4 +10,6 @@ struct MenuItem: Codable, Identifiable {
     let id = UUID()
     
     // add code here
+    let title: String
+    let price: Double
 }
